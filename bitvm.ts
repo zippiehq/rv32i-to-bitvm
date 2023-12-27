a4da17a88ddd43b9458ae84abbe00f6a21c79221
Carsten Munk <carsten@zippie.com>
2023-12-27 22:24:43 +0100

    WIP
    Signed-off-by: Carsten Munk <carsten@zippie.com>

---
+++ bitvm.ts
@@ -0,0 +1,198 @@
+
+export const ASM_ADD = 42 // *aC = (*aA + *aB) & (2^32-1)
+export const ASM_SUB = 43 // *aC = (*aA + *aB) & (2^32-1)
+//export const ASM_MUL = 44
+export const ASM_JMP = 45 // pc = *aA
+export const ASM_BEQ = 46 // if *aA == *aB: pc = aC
+export const ASM_BNE = 47 // if *aA != *aB: pc = aC
+
+// new
+export const ASM_ADDI = 48 // *aC = (*aA + aB) & (2^32-1)
+export const ASM_SUBI = 49 // *aC = (*aA - aB) & (2^32-1)
+export const ASM_XORI = 50 // *aC = (*aA ^ aB) ^ (2^32-1)
+export const ASM_XOR = 51  // *aC = (*aA ^ *aB) ^ (2^32-1)
+export const ASM_OR = 52 // *aC = (*aA | *aB) ^ (2^32-1)
+export const ASM_AND = 53 // *aC = (*aA & *aB) ^ (2^32-1)
+export const ASM_ANDI = 54
+export const ASM_ORI = 55
+export const ASM_RSHIFT1 = 56
+export const ASM_SLTU = 57
+export const ASM_SLT = 58
+
+export class Instruction {
+    
+    constructor(type, addressA, addressB, addressC) {
+        this.type = type
+        this.addressA = addressA
+        this.addressB = addressB
+        this.addressC = addressC
+    }
+
+    toString() {
+        return `${this.type} ${this.addressA} ${this.addressB} ${this.addressC}`
+    }
+}
+
+export const compileProgram = source => source.map(instruction => new Instruction(...instruction))
+
+class Snapshot {
+    pc
+    memory
+    stepCount = 0
+    instruction
+
+    constructor(memory, instruction, pc = 0) {
+        this.memory = memory
+        this.instruction = instruction
+        this.pc = pc
+    }
+
+    read(address) {
+        if(address < 0) 
+            throw `ERROR: address=${address} is negative`
+        if(address >= this.memory.length) 
+            throw `ERROR: address=${address} >= memory.length=${this.memory.length}`
+        return this.memory[address]
+    }
+
+    write(address, value) {
+        if(address < 0) 
+            throw `ERROR: address=${address} is negative`
+        if(address >= this.memory.length) 
+            throw `ERROR: address=${address} >= memory.length=${this.memory.length}`
+        this.memory[address] = value
+    }
+}
+
+const executeInstruction = (snapshot) => {
+
+    // console.log(`PC: ${snapshot.pc},  Instruction: ${(snapshot.instruction+'').padEnd(9,' ')} Memory: [${snapshot.memory}]`)
+    switch (snapshot.instruction.type) {
+        case ASM_ADD:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) + snapshot.read(snapshot.instruction.addressC)) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_SUB:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) - snapshot.read(snapshot.instruction.addressC)) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_XOR:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) ^ snapshot.read(snapshot.instruction.addressC)) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_BEQ:
+            if (snapshot.read(snapshot.instruction.addressA) == snapshot.read(snapshot.instruction.addressB)) {
+                snapshot.pc = snapshot.instruction.addressC
+            } else {
+                snapshot.pc += 1
+            }
+            break
+        case ASM_BNE:
+            if (snapshot.read(snapshot.instruction.addressA) != snapshot.read(snapshot.instruction.addressB)) {
+                snapshot.pc = snapshot.instruction.addressC
+            } else {
+                snapshot.pc += 1
+            }
+            break
+        case ASM_JMP:
+            snapshot.pc = snapshot.read(snapshot.instruction.addressA)
+            break
+        case ASM_ADDI:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) + snapshot.instruction.addressC) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_SUBI:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) - snapshot.instruction.addressC) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_XORI:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) ^ snapshot.instruction.addressC) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_AND:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressA) & snapshot.read(snapshot.instruction.addressB)) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_OR:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) | snapshot.read(snapshot.instruction.addressC)) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_ANDI:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) & snapshot.instruction.addressC) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_ORI:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) | snapshot.instruction.addressC) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_RSHIFT1:
+            snapshot.write(
+                snapshot.instruction.addressA,
+                (snapshot.read(snapshot.instruction.addressB) >>> 1) & 0xFFFFFFFF
+            )
+            snapshot.pc += 1
+            break
+        case ASM_SLTU:
+            snapshot.write(snapshot.instruction.addressA, snapshot.read(snapshot.instruction.addressB) >>> 0 < snapshot.read(snapshot.instruction.addressC) >>> 0 ? 1 : 0))
+            snapshot.pc += 1
+            break            
+        case ASM_SLT:
+            snapshot.write(snapshot.instruction.addressA, snapshot.read(snapshot.instruction.addressB) < snapshot.read(snapshot.instruction.addressC) ? 1 : 0))
+            snapshot.pc += 1
+            break            
+        default:
+            snapshot.pc += 1
+            break
+    }
+}
+
+export class VM {
+    program
+    memoryEntries
+
+    constructor(programSource, memoryEntries) {
+        this.program = compileProgram(programSource)
+        this.memoryEntries = memoryEntries
+    }
+
+    // essentially if length runs out, machine halts
+    run(maxSteps = TRACE_LEN) {
+        const snapshot = new Snapshot([...this.memoryEntries], this.program[0])
+        while (snapshot.pc < this.program.length && snapshot.stepCount < maxSteps) {
+            snapshot.instruction = this.program[snapshot.pc]
+            executeInstruction(snapshot)
+            snapshot.stepCount++
+        }
+        return snapshot
+    }
+}
--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 import { Instruction, parseInstruction } from "./instructionParser";
-import {
-  addProgramCounters,
-  performAssembly,
-  resolveNamesAndOffsets,
-} from "./util";
 import * as elfinfo from "elfinfo";
 import crypto from "crypto";
 import fs from "fs";
+import * as bitvm from "./bitvm";
 
-function emitRiscv(
+/*function emitRiscv(
   opcodes: EVMOpCode[],
   parsed: Instruction,
   startPc: number,
@@ -144,8 +140,8 @@ function emitRiscv(
       Opcodes.emitSlt(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
       break;
     }
+    case "SLTU"
 
-    case "SLTU": {
       Opcodes.emitSltu(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
       break;
     }
@@ -275,7 +271,6 @@ function emitRiscv(
     case "SD":
       Opcodes.emitSd(opcodes, parsed.rs1, parsed.rs2, parsed.imm);
       break;
-    /* M extension */
     case "MUL":
       Opcodes.emitMul(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
       break;
@@ -320,7 +315,7 @@ function emitRiscv(
       Opcodes.emitDirtyCheck(opcodes, pc);
       Opcodes.emitLr(opcodes, parsed.rd, parsed.rs1, pc, parsed.instructionName);
       break;
-    case "SC.W":
+   case "SC.W":
     case "SC.D":
       Opcodes.emitSc(opcodes, parsed.rd, parsed.rs1, parsed.rs2, pc, parsed.instructionName);
       break;
@@ -354,13 +349,500 @@ function printO(cycle: number, op: EVMOpCode) {
       (op.comment ? " ;; # " + op.comment : "")
   );
 }
+*/
 
+/* memory layout
+
+  0 = always 0
+  4..4*32 = x1..x32
+  code page has been replaced with 32-bit jump offsets into bitvm instead of code
+*/
+
+export interface BitVMOpcode {
+  opcode: bitvm.Instruction;
+  pc?: number;
+  label?: string;
+  find_label?: string;
+  find_riscv_pc?: number;
+  find_target?: string; // addressA, addressB, or addressC -- where to write resolved label to in instruction
+}
+
+function reg2mem(reg: number) {
+   return reg; // in future, * 4   
+}
+
+function tmp() { return 33; }
+function tmp2() { return 34; }
+function tmp3() { return 35; }
+
+function emitBitvmOp(opcodes: BitVMOpcode[], op: number, addressA: number, addressB: number, addressC: number) {
+   opcodes.push({ opcode: new bitvm.Instruction(op, addressA, addressB, addressC) });
+}
+
+function emitRiscvADD(opcodes: BitVMOpcode[], rd: number, rs1: number, rs2: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_ADD, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+   }
+}
+
+function emitRiscvADDI(opcodes: BitVMOpcode[], rd: number, rs1: number, imm: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(rs1), imm);
+   }
+}
+
+function emitRiscvSUB(opcodes: BitVMOpcode[], rd: number, rs1: number, rs2: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_SUB, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+   }
+}
+
+function emitRiscvXOR(opcodes: BitVMOpcode[], rd: number, rs1: number, rs2: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_XOR, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+   }
+}
+
+function emitRiscvXORI(opcodes: BitVMOpcode[], rd: number, rs1: number, imm: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_XORI, reg2mem(rd), reg2mem(rs1), imm);
+   }
+}
+
+function emitRiscvAND(opcodes: BitVMOpcode[], rd: number, rs1: number, rs2: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_AND, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+   }
+}
+
+function emitRiscvANDI(opcodes: BitVMOpcode[], rd: number, rs1: number, imm: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_ANDI, reg2mem(rd), reg2mem(rs1), imm);
+   }
+}
+
+function emitRiscvOR(opcodes: BitVMOpcode[], rd: number, rs1: number, rs2: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_OR, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+   }
+}
+
+function emitRiscvORI(opcodes: BitVMOpcode[], rd: number, rs1: number, imm: number) {
+   if (rd != 0) { 
+     emitBitvmOp(opcodes, bitvm.ASM_ORI, reg2mem(rd), reg2mem(rs1), imm);
+   }
+}
+
+function emitJAL(opcodes: BitVMOpcode[], rd: number, imm: number, riscv_pc: number) {
+   if (rd != 0) {
+     emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(0), riscv_pc + 4);
+   }
+   opcodes.push({ opcode: new bitvm.Instruction(bitvm.ASM_BEQ, reg2mem(0), reg2mem(0), 0) }, find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+}
+
+function emitJALR(opcodes: BitVMOpcode[], rd: number, imm: number, riscv_pc: number) {
+   if (rd != 0) {
+     emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(0), riscv_pc + 4);
+   }
+   emitBitVmop(opcodes, bitvm.ASM_ADDI, tmp(), reg2mem(rs1), imm);
+   emitBitVmop(opcodes, bitvm.ASM_JMP, tmp(), 0, 0);
+}
+
+function emitAUIPC(opcodes: BitVMOpcode[], rd: number, imm: number, riscv_pc: number) {
+   if (rd != 0) {
+     emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(0), (riscv_pc + (imm << 12)) & 0xFFFFFFFF);
+   }
+}
+
+function emitLUI(opcodes: BitVMOpcode[], rd: number, imm: number) {
+   if (rd != 0) {
+     emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(0), (imm << 12) & 0xFFFFFFFF);
+   }
+}
+
+function emitBEQ(opcodes: BitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+   opcodes.push({ opcode: new bitvm.Instruction(bitvm.ASM_BEQ, reg2mem(rs1), reg2mem(rs2),  0),  find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+}
+
+function emitBNE(opcodes: BitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+   opcodes.push({ opcode: new bitvm.Instruction(bitvm.ASM_BNE, reg2mem(rs1), reg2mem(rs2), 0), find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+}
+
+function emitSLLI(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(rs1), 0);
+      for (let i = 0; i < imm; i++) {
+       emitBitvmOp(opcodes, bitvm.ASM_ADD, reg2mem(rd), reg2mem(rd), reg2mem(rd));
+      }   
+    }
+}
+
+function emitSRLI(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_ADDI, reg2mem(rd), reg2mem(rs1), 0);
+      for (let i = 0; i < imm; i++) {
+       emitBitvmOp(opcodes, bitvm.ASM_RSHIFT1, reg2mem(rd), reg2mem(rd), 0);
+      }   
+    }
+}
+
+function emitSLT(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLT, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+    }
+}
+
+function emitSLTU(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLTU, reg2mem(rd), reg2mem(rs1), reg2mem(rs2));
+    }
+}
+
+function emitSLTIU(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_ADDI, tmp(), reg2mem(0), imm);
+      emitBitvmOp(opcodes, bitvm.ASM_SLTU, reg2mem(rd), reg2mem(rs1), tmp());
+    }
+}
+
+function emitSLTI(opcodes: bitVMOpcode[], rd: number, rs1: number, imm: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_ADDI, tmp(), reg2mem(0), imm);
+      emitBitvmOp(opcodes, bitvm.ASM_SLT, reg2mem(rd), reg2mem(rs1), tmp());
+    }
+}
+
+function emitBLT(opcodes: bitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLT, tmp(), reg2mem(rs1), reg2mem(rs2));
+      opcodes.push({ opcode: { type: bitvm.ASM_BEQ, addressA: tmp(), addressB: reg2mem(0), addressC: 0 }, find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+    }
+}
+
+function emitBLTU(opcodes: bitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLTU, tmp(), reg2mem(rs1), reg2mem(rs2));
+      opcodes.push({ opcode: { type: bitvm.ASM_BEQ, addressA: tmp(), addressB: reg2mem(0), addressC: 0 }, find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+    }
+}
+
+function emitBGE(opcodes: bitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLT, tmp(), reg2mem(rs1), reg2mem(rs2));
+      opcodes.push({ opcode: { type: bitvm.ASM_BNE, addressA: tmp(), addressB: reg2mem(0), addressC: 0 }, find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+    }
+}
+
+function emitBGEU(opcodes: bitVMOpcode[], rs1: number, rs2: number, imm: number, riscv_pc: number) {
+    if (rd != 0) {
+      emitBitvmOp(opcodes, bitvm.ASM_SLTU, tmp(), reg2mem(rs1), reg2mem(rs2));
+      opcodes.push({ opcode: { type: bitvm.ASM_BNE, addressA: tmp(), addressB: reg2mem(0), addressC: 0 }, find_riscv_pc: (riscv_pc + imm) & 0xFFFFFFFF, find_target: "addressC"});
+    }
+}
+
+function emitECALL(opcodes: bitVMOpcode[]) {
+    // tmp() acts as our status buffer, 0 = OK, 1 = not OK
+    emitBitVmOp(opcodes, bitvm.ADDI, tmp(), reg2mem(0), 0);
+    opcodes.push({ opcode: { type: bitvm.ASM_BEQ, addressA: reg2mem(0), addressB: reg2mem(0), addressC: 0 }, find_label: "_program_end", find_target: "addressC"});
+}
+
+function emitEBREAK(opcodes: bitVMOpcode[]) {
+    emitBitVmOp(opcodes, bitvm.ADDI, tmp(), reg2mem(0), 1);
+    opcodes.push({ opcode: { type: bitvm.ASM_BEQ, addressA: reg2mem(0), addressB: reg2mem(0), addressC: 0 }, find_label: "_program_end", find_target: "addressC"});
+}
+
+
+
+/*
+  transpilations:
+
+  risc-v SLL rd, rs1, rs2
+     bitVM_ADDI(tmp(), reg2mem(rs1), 0) # result
+     bitVM_ADDI(tmp(), reg2mem(rs2), 0) # shift amount
+     
+     _SLL_UNIQ_loop_start:
+     # check if shift amount is zero
+     bitVM_BEQ(reg2mem(0), tmp(), sll_UNIQ_loop_end)
+     
+     # Double the value by adding it to itself
+
+     bitVM_ADD(tmp(), tmp(),  tmp())
+     bitVM_SUBI(tmp2(), tmp2(), 1)
+    
+     bitVM_BEQ(reg2mem(0), reg2mem(0), sll_UNIQ_loop_start)
+     
+     sll_UNIQ_loop_end:
+     if (rd != 0) 
+       bitVM_ADDI(reg2mem(rd), tmp(), 0)
+   
+   risc-v SRL rd, rs1, rs2
+     bitVM_ADDI(tmp(), reg2mem(rs1), 0) # result
+     bitVM_ADDI(tmp(), reg2mem(rs2), 0) # shift amount
+     
+     _SRL_UNIQ_loop_start:
+     # check if shift amount is zero
+     bitVM_BEQ(reg2mem(0), tmp(), sll_UNIQ_loop_end)
+     
+     # Call RSHIFT1
+     bitVM_RSHIFT1(tmp(), tmp())
+     bitVM_SUBI(tmp2(), tmp2(), 1)
+    
+     bitVM_BEQ(reg2mem(0), reg2mem(0), srl_UNIQ_loop_start)
+     
+     srl_UNIQ_loop_end:
+     if (rd != 0) 
+       bitVM_ADDI(reg2mem(rd), tmp(), 0)
+
+   risc-v SRA rd, rs1, rs2
+     bitVM_ADDI(tmp(), reg2mem(rs1), 0) # result
+     bitVM_ADDI(tmp2(), reg2mem(rs2), 0) # shift amount
+     bitVM_ANDI(tmp3(), reg2mem(rs1), 0x80000000) # MSB
+     
+     _SRA_UNIQ_loop_start:
+     # check if shift amount is zero
+     bitVM_BEQ(reg2mem(0), tmp(), sra_UNIQ_loop_end)
+     
+     # Call RSHIFT1
+     bitVM_RSHIFT1(tmp(), tmp())
+     bitVM_OR(tmp(), tmp(), tmp3())  # add MSB
+     bitVM_SUBI(tmp2(), tmp2(), 1)
+    
+     bitVM_BEQ(reg2mem(0), reg2mem(0), srl_UNIQ_loop_start)
+     
+     srl_UNIQ_loop_end:
+     if (rd != 0) 
+       bitVM_ADDI(reg2mem(rd), tmp(), 0)
+
+    risc-v SRAI rd, rs1, imm:
+    
+     if (rd != 0): 
+        bitVM_ADDI(reg2mem(rd), reg2mem(rs1), 0)
+        bitVM_ANDI(tmp3(), reg2mem(rs1), 0x80000000) # MSB
+        repeat imm times:  
+          bitVM_RSHIFT1(reg2mem(rd), reg2mem(rd))
+          bitVM_OR(reg2mem(rd), reg2mem(rs1), tmp3()) // extend with MSB
+                 
+*/
+  
+function emitInstr(opcodes: BitVMOpcode[], pc: number, parsed: Instruction) {
+  switch (parsed.instructionName) {
+    case "SLL":
+      emitSLL(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.rs2
+      );
+    case "SRL": {
+      emitSRL(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.rs2
+      );
+      break;
+    }
+    case "SLLI": {
+       emitSLLI(
+        opcodes,
+        parsed.instructionName,
+        parsed.rd,
+        parsed.rs1,
+        parsed.imm
+      );
+      break;
+
+    case "SRLI": {
+      emitSRLI(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.imm
+      );
+      break;
+    }
+    case "SRA": {
+      emitSRA(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
+      break;
+    }
+    case "SRAI": {
+      emitSRAI(opcodes, parsed.rd, parsed.rs1, parsed.imm);
+      break;
+    }
+    // arithmetic
+    case "ADD": {
+      emitADD(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
+      break;
+    }
+    case "ADDI": {
+      emitADDI(opcodes, parsed.rd, parsed.rs1, parsed.imm);
+      break;
+    }
+    case "SUB": {
+      emitSUB(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
+      break;
+    }
+    case "LUI": {
+      emitLUI(opcodes, parsed.rd, parsed.unparsedInstruction);
+      break;
+    }
+    case "AUIPC": {
+      emitAUIPC(opcodes, parsed.rd, parsed.imm, pc);
+      break;
+    }
+    case "OR": {
+      emitOR(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.rs2
+      );
+    }
+    case "XOR": {
+      emitXOR(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.rs2
+      );
+    }
+    case "AND": {
+      emitAND(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.rs2
+      );
+      break;
+    }
+    case "ORI": {
+      emitORI(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.imm
+      );
+      break;
+    }
+    case "XORI": {
+      emitXORI(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.imm
+      );
+      break;
+    }
+    case "ANDI": {
+      emitANDI(
+        opcodes,
+        parsed.rd,
+        parsed.rs1,
+        parsed.imm
+      );
+      break;
+    }
+    // compare
+    case "SLT": {
+      emitSLT(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
+      break;
+    }
+    case "SLTU"
+
+      emitSLTU(opcodes, parsed.rd, parsed.rs1, parsed.rs2);
+      break;
+    }
+
+    case "SLTI": {
+      emitSLTI(opcodes, parsed.rd, parsed.rs1, parsed.imm);
+      break;
+    }
+
+    case "SLTIU": {
+      emitSLTIU(opcodes, parsed.rd, parsed.rs1, parsed.imm);
+      break;
+    }
+    // branches
+    case "BNE":
+      emitBNE(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    case "BEQ":
+      emitBEQ(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    case "BLT":
+      emitBLT(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    case "BGE":
+      emitBGE(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    case "BLTU":
+      emitBLTU(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    case "BGEU":
+      emitBGEU(opcodes, parsed.rs1, parsed.rs2, parsed.imm, pc);
+      break;
+    // jump & link
+    case "JAL": {
+      emitJAL(opcodes, parsed.rd, parsed.imm, pc);
+      break;
+    }
+    case "JALR": {
+      emitJALR(opcodes, parsed.rd, parsed.rs1, parsed.imm, pc);
+      break;
+    }
+    // Synch (do nothing, single-thread)
+    case "FENCE":
+    case "FENCE.I":
+      break;
+    // environment
+    case "EBREAK":
+      emitEBREAK(opcodes);
+      break;
+    case "ECALL":
+      emitECALL(opcodes);
+      break;
+    default:
+      throw new Error("Unknown instruction: " + parsed.instructionName);
+  }
+}
+
+
+function riscvToBitVM(pc_base: number, buf: Buffer): BitVMOpcode[] {
+  let opcodes: BitVMOpcode[] = [];
+  for (let i = 0; i < buf.length; i += 4) {
+    const instr = buf.readUInt32LE(i);
+    const parsed = parseInstruction(instr);
+    const instrName = parsed.instructionName;
+
+    console.log(parsed);
+    emitInstr(opcodes, pc_base + i, parsed);
+  }
+  return opcodes;
+}
+
+
+
+/* phases:
+   transpile
+   add program counters
+   resolve labels to pc (one instruction per pc location)
+   emit instruction list & memory contents
+   run in bitvm
+*/
+   
 async function transpile(fileContents: Buffer) {
   const elfInfo = await elfinfo.open(fileContents);
   if (!elfInfo || !elfInfo.elf) {
     throw new Error("No ELF");
   }
 
+  let context = {
+     codepage: Buffer.alloc(0),
+     code_addr: 0,
+     datapage: Buffer.alloc(0),
+     data_addr: 0
+  }
+  
   for (let i = 0; i < elfInfo.elf.segments.length; i++) {
     const seg = elfInfo.elf.segments[i];
     if (
@@ -376,20 +858,8 @@ async function transpile(fileContents: Buffer) {
         throw new Error("Code segment should start at 0x20000000");
       }
       const data = fileContents.slice(seg.offset, seg.offset + seg.filesz);
-      for (let page = 0; page < data.length; page += 1024) {
-        const [code, opcodes] = await makePageCode(
-          Number(seg.vaddr) + (page & (0xfffffc00 >>> 0)),
-          data.slice(page, page + 1024)
-        );
-        context.pages.push({
-          ethAddress: Address.fromString(
-            "0x" + crypto.randomBytes(20).toString("hex")
-          ),
-          addr: Number(seg.vaddr) + page,
-          code: code,
-          opcodes: opcodes,
-        });
-      }
+      context.codepage = data;
+      context.code_addr = Number(seg.vaddr)
     } else if (
       seg.vaddr !== 0 &&
       seg.typeDescription == "Load" &&
@@ -399,21 +869,12 @@ async function transpile(fileContents: Buffer) {
         throw new Error("Segment should be 4K-aligned");
       }
       const data = fileContents.slice(seg.offset, seg.offset + seg.filesz);
-      for (let page = 0; page < data.length; page += 16384) {
-        context.dataPages.push({
-          ethAddress: Address.fromString(
-            "0x" + crypto.randomBytes(20).toString("hex")
-          ),
-          addr: Number(seg.vaddr) + page,
-          code: data.slice(page, page + 16384,
-          opcodes: [],
-        });
-      }
+      context.datapage = data;
+      context.data_addr = Number(seg.vaddr)
     }
-  }
-
-  console.log(context);
-
+   }
+   console.log(context);
+   console.log(riscvToBitVM(context.codepage));
 }
 
 transpile(fs.readFileSync(process.argv[2])).catch((err) => {
